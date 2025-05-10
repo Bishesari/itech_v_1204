2af32891ac424f5c0d0116feb58b3ffe759bb8ad
Yasser <Yasser.Bishesari@gmail.com>
2025-05-11 01:05:34 +0330

    Vivo Book

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -15,6 +15,7 @@
                 <flux:navlist.group :heading="__('سکوی توسعه')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('داشبرد') }}</flux:navlist.item>
                 </flux:navlist.group>
+
             </flux:navlist>
 
             <flux:spacer />
