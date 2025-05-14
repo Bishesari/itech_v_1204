82e726d49da308ee65483c9c96019c821f63dfc2
SiteD-Server <Yasser.Bishesari@gmail.com>
2025-05-14 15:04:54 +0330

    C-Server

--- resources/views/livewire/institutes/index.blade.php
+++ resources/views/livewire/institutes/index.blade.php
@@ -7,5 +7,8 @@ new class extends Component {
 }; ?>
 
 <section class="w-full">
-@include('partials.settings-heading')
+    @include('livewire.institutes.heading')
+    <x-institutes.layout :heading="__('Profile')" :subheading="__('Update your name and email address')">
+
+    </x-institutes.layout>
 </section>
