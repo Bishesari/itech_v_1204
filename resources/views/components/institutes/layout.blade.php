8617609fac258b165ce9eae095f54930874fd629
SiteD-Server <Yasser.Bishesari@gmail.com>
2025-05-15 10:09:31 +0330

    C-Server

--- resources/views/components/institutes/layout.blade.php
+++ resources/views/components/institutes/layout.blade.php
@@ -1,5 +1,5 @@
 <div class="flex items-start max-md:flex-col">
-    <div class="me-10 w-full pb-4 md:w-[220px]">
+    <div class="me-10 w-full pb-4 md:w-[220px] ">
         <flux:navlist>
             <flux:navlist.item :href="route('settings.profile')" wire:navigate>{{ __('Index') }}</flux:navlist.item>
             <flux:navlist.item :href="route('settings.password')" wire:navigate>{{ __('Password') }}</flux:navlist.item>
@@ -13,7 +13,7 @@
         <flux:heading>{{ $heading ?? '' }}</flux:heading>
         <flux:subheading>{{ $subheading ?? '' }}</flux:subheading>
 
-        <div class="mt-5 w-full max-w-lg">
+        <div class="mt-5 w-full">
             {{ $slot }}
         </div>
     </div>
--- resources/views/livewire/institutes/index.blade.php
+++ resources/views/livewire/institutes/index.blade.php
@@ -20,10 +20,34 @@ new class extends Component {
 <section class="w-full">
     @include('livewire.institutes.heading')
     <x-institutes.layout :heading="__('Profile')" :subheading="__('Update your name and email address')">
+        <table class="text-sm text-center mx-auto w-[1050px]">
+            <tr>
+                <th class="border w-[50px]">{{__('#')}}</th>
+                <th class="border w-[100px]">{{__('نام کوتاه')}}</th>
+                <th class="border w-[250px]">{{__('نام کامل')}}</th>
+                <th class="border w-[150px]">{{__('نام اختصاری')}}</th>
+                <th class="border w-[150px]">{{__('مانده اعتبار')}}</th>
+                <th class="border w-[150px]">{{__('لوگو')}}</th>
+                <th class="border w-[100px]">{{__('تاریخ درج')}}</th>
+                <th class="border w-[100px]">{{__('تاریخ ویرایش')}}</th>
+            </tr>
+            @foreach($institutes as $institute)
+            <tr>
+                <td class="border">{{$institute->id}}</td>
+                <td class="border">{{$institute->short_name}}</td>
+                <td class="border">{{$institute->full_name}}</td>
+                <td class="border">{{$institute->abb}}</td>
+                <td class="border">{{$institute->remain_credit}}</td>
+                <td class="border">{{$institute->logo_url}}</td>
+                <td class="border">{{$institute->created}}</td>
+                <td class="border">{{$institute->updated}}</td>
+            </tr>
+            @endforeach
+        </table>
 
-        @foreach($institutes as $institute)
-            {{$id}}
-        @endforeach
+
+
+
 
     </x-institutes.layout>
 </section>
